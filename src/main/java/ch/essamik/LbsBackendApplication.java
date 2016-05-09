25fec2734b41084d729b5436bdeee3f653fa2e90
Karim Es-sami <karim.es-sami@swisscom.com>
2016-05-09 11:21:53 +0200

    Initial commit with gradle plugin as build system

---
+++ src/main/java/ch/essamik/LbsBackendApplication.java
@@ -0,0 +1,12 @@
+package ch.essamik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbsBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/ch/essamik/LbsBackendApplicationTests.java
@@ -0,0 +1,18 @@
+package ch.essamik;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LbsBackendApplication.class)
+@WebAppConfiguration
+public class LbsBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
