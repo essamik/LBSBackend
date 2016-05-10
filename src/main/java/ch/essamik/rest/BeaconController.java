17bcfd8774db793ea2f6ebffe12a33c2c538d694
Karim Es-sami <karim.es-sami@swisscom.com>
2016-05-10 16:32:16 +0200

    First working implementation of a get beacon by name web service

--- src/main/java/ch/essamik/rest/BeaconController.java
+++ src/main/java/ch/essamik/rest/BeaconController.java
@@ -1,8 +1,25 @@
 package ch.essamik.rest;
 
+import ch.essamik.model.Beacon;
+import ch.essamik.repositories.BeaconRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * REST endpoint for Beacon
  */
+
+@RestController
+@RequestMapping("/beacons")
 public class BeaconController {
 
+    @Autowired
+    private BeaconRepository beaconRepository;
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public Beacon getByName(@PathVariable String name) {
+        Beacon foundBeacon = beaconRepository.findByName(name);
+
+        return foundBeacon;
+    }
 }
